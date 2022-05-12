886adb644c048d9184cc83d212eadcca3b2b4343
Kacper Wiączek <kacper.wiaczek00@gmail.com>
2022-05-12 15:23:49 +0200

    gui 1 0

--- src/main/java/com/wiaczek/socki/graphalgogui/HelloApplication.java
+++ src/main/java/com/wiaczek/socki/graphalgogui/HelloApplication.java
@@ -2,7 +2,9 @@ package com.wiaczek.socki.graphalgogui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth()/1.5, screenBounds.getHeight()/1.5);
+        stage.setTitle("Graphalgo-gui");
         stage.setScene(scene);
         stage.show();
     }
